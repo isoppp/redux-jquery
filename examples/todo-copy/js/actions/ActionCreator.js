1c14144625e8ca26366af310ab05d575ffc15a6c
礒谷 浩生 <hiroki_isogai@waku-2.com>
2016-04-21 17:01:27 +0900

    arrow function 書式修正

--- examples/todo-copy/js/actions/ActionCreator.js
+++ examples/todo-copy/js/actions/ActionCreator.js
@@ -1,19 +1,19 @@
 import {createAction} from 'redux-actions';
 
 export const ADD_TODO = 'ADD_TODO';
-export const addTodo = createAction(ADD_TODO, (title)=> title);
+export const addTodo = createAction(ADD_TODO, (title) => title);
 
 export const DELETE_TODO = 'DELETE_TODO';
-export const deleteTodo = createAction(DELETE_TODO, (id)=> id);
+export const deleteTodo = createAction(DELETE_TODO, (id) => id);
 
 export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const completeTodo = createAction(COMPLETE_TODO, (id)=> id);
+export const completeTodo = createAction(COMPLETE_TODO, (id) => id);
 
 export const ACTIVE_TODO = 'ACTIVE_TODO';
-export const activeTodo = createAction(ACTIVE_TODO, (id)=> id);
+export const activeTodo = createAction(ACTIVE_TODO, (id) => id);
 
 export const FILTER_TODO = 'FILTER_TODO';
-export const filterTodo = createAction(FILTER_TODO, (filterName)=> filterName);
+export const filterTodo = createAction(FILTER_TODO, (filterName) => filterName);
 
 export const INITIALIZE = 'INITIALIZE';
 export const initialize = createAction(INITIALIZE, (todoObjects) => todoObjects);
